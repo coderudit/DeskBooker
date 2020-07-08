69bd9dda79f790a0fdca55fd17fb1bbe751a6274
gitName <a@b.c>
2020-07-08 08:49:20 +0530

    Add project files.

---
+++ DeskBooker.Core/Processor/DeskBookingRequestProcessor.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using DeskBooker.Core.DataInterface;
+using DeskBooker.Core.Domain;
+
+namespace DeskBooker.Core.Processor
+{
+    public class DeskBookingRequestProcessor
+    {
+        private readonly IDeskBookingRepository _deskBookingRepository;
+        private readonly IDeskRepository _deskRepository;
+
+        public DeskBookingRequestProcessor(IDeskBookingRepository deskBookingRepository, IDeskRepository deskRepository)
+        {
+            this._deskBookingRepository = deskBookingRepository;
+            this._deskRepository = deskRepository;
+        }
+
+        public DeskBookingResult BookDesk(DeskBookingRequest request)
+        {
+            if (request == null)
+            {
+                throw new ArgumentNullException(nameof(request));
+            }
+            var result = Create<DeskBookingResult>(request);
+            var availableDesks = _deskRepository.GetAvailableDesks(request.Date);
+            if (availableDesks.FirstOrDefault() is Desk firstAvailableDesk)
+            {
+                var deskBooking = Create<DeskBooking>(request);
+                deskBooking.DeskId = firstAvailableDesk.Id;
+                _deskBookingRepository.Save(deskBooking);
+                result.DeskBookingId = deskBooking.Id;
+                result.Code = DeskBookingResultCode.Success;
+            }
+            else
+            {
+                result.Code = DeskBookingResultCode.NoDeskAvailable;
+            }
+            return result;
+        }
+
+        private static T Create<T>(DeskBookingRequest request) where T : DeskBookingBase, new()
+        {
+            return new T
+            {
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Email = request.Email,
+                Date = request.Date
+            };
+        }
+    }
+}
